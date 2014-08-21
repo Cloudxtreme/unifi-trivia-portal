74a1445620a12e7173425f66f7c4bf9b0996d96d
Aaron Hopkins <github@die.net>
2014-08-21 07:11:10 -0700

    Add mysql connection setup.

--- main.go
+++ main.go
@@ -1,33 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"github.com/die-net/unifi"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"runtime"
 )
 
 var (
-	host    = flag.String("host", "unifi", "Controller hostname")
-	user    = flag.String("user", "admin", "Controller username")
-	pass    = flag.String("pass", "unifi", "Controller password")
-	version = flag.Int("version", 2, "Controller base version")
-	siteid  = flag.String("siteid", "default", "Site ID, UniFi v3 only")
-	listen  = flag.String("listen", ":8000", "The [IP]:port to listen for incoming connections on.")
-	threads = flag.Int("threads", runtime.NumCPU(), "The number of worker threads to execute.")
+	host              = flag.String("host", "unifi", "Controller hostname")
+	user              = flag.String("user", "admin", "Controller username")
+	pass              = flag.String("pass", "unifi", "Controller password")
+	version           = flag.Int("version", 2, "Controller base version")
+	siteid            = flag.String("siteid", "default", "Site ID, UniFi v3 only")
+	listen            = flag.String("listen", ":8000", "The [IP]:port to listen for incoming connections on.")
+	threads           = flag.Int("threads", runtime.NumCPU(), "The number of worker threads to execute.")
+	mysqlDsn          = flag.String("mysql_dsn", "utp:@/utp?timeout=2s&parseTime=true&wait_timeout=300", "MySQL connection DSN")
+	mysqlMaxIdleConns = flag.Int("mysql_max_idle_conns", 5, "Maximum number of idle MySQL connections to allow.")
+	db                *sql.DB
+	ufi               *unifi.Unifi
 )
 
 func main() {
-	runtime.GOMAXPROCS(*threads)
-
 	flag.Parse()
 
-	u, err := unifi.Login(*user, *pass, *host, *siteid, *version)
+	runtime.GOMAXPROCS(*threads)
+
+	var err error
+	db, err = sql.Open("mysql", *mysqlDsn)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer u.Logout()
+	db.SetMaxIdleConns(*mysqlMaxIdleConns)
+	defer db.Close()
+
+	ufi, err = unifi.Login(*user, *pass, *host, *siteid, *version)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ufi.Logout()
 
 	log.Fatal(http.ListenAndServe(*listen, nil))
 }
